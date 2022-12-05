cdb61225e9ae11a5e932c6fafc09e9e11cc0fb46
auliafaizahr <zahaulia@gmail.com>
2022-12-05 23:02:46 +0700

    05. Creating our Header and Nav Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children, cool }) {
   return (
     <div>
+      <Header />
       <p>Hello! This is page components hehehe</p>
       {children}
       <p>Halo yang ini {cool} </p>
