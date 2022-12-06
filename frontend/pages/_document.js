d7bf3de7ac4cf00c66d91da42d42f322b856f188
auliafaizahr <zahaulia@gmail.com>
2022-12-06 20:24:37 +0700

    06. Intro to Styled Component and CSS

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,15 +1,15 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
-  render(){
-    return(
-    <Html lang="en-CA">
-      {/* <Head></Head> */}
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+  render() {
+    return (
+      <Html lang="en-CA">
+        {/* <Head></Head> */}
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     );
   }
 }
