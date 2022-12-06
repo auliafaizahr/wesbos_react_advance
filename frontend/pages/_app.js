7e01ad0d83176ee58cf7324e6bf5e8f61d7520cc
auliafaizahr <zahaulia@gmail.com>
2022-12-06 21:35:32 +0700

    08. Visualizing Route Changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,14 @@
+import Nprogress from 'nprogress'; // For displaying the progress bar
+import Router from 'next/router'; // For handling all the changes in routes/router
 import Page from '../components/Page';
+
+// TODO : swap with our own
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start()); // fires up when a route starts to change; in other words, when a user navigates to a new page.
+Router.events.on('routeChangeComplete', () => Nprogress.done()); // is triggered when a route changes completely:
+Router.events.on('routeChangeError', () => Nprogress.done()); // This event is triggered when a route fails to change.
 
 export default function MyApp({ Component, pageProps }) {
   return (
